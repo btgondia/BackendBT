e7e92258ba58a5d30f93e58a579f191897c1eaa2
M Waseem <wasirana80@gmail.com>
2022-05-14 18:05:45 +0500

    Update Item.js

--- Routes/Item.js
+++ Routes/Item.js
@@ -38,4 +38,24 @@ router.get("/GetItemList", async (req, res) => {
   }
 });
 
+router.put("/putItem", async (req, res) => {
+  try {
+    let value = req.body;
+    if (!value) res.json({ success: false, message: "Invalid Data" });
+   value= Object.keys(value)
+   .filter((key) => key !== "_id")
+   .reduce((obj, key) => {
+     obj[key] = value[key];
+     return obj;
+   }, {})
+    console.log(value);
+    let response = await Item.updateOne({item_uuid:value.item_uuid}, value );
+    if (response.acknowledged) {
+      res.json({ success: true, result: value });
+    } else res.json({ success: false, message: "Item Not created" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+
 module.exports = router;
