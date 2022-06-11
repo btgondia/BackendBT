2fccb05ed4acd47cd2854497148b9a3e7a8a2a22
M Waseem <wasirana80@gmail.com>
2022-06-11 22:42:52 +0500

    Update Orders.js

--- Routes/Orders.js
+++ Routes/Orders.js
@@ -92,7 +92,8 @@ router.put("/putOrders", async (req, res) => {
         
       console.log(value, orderStage);
 
-     if (+orderStage === 4 || +orderStage === 5||value?.item_details?.length) {
+     if (+orderStage === 4 || +orderStage === 5||!value?.item_details?.length) {
+       console.log(+orderStage, value?.item_details?.length)
         await Orders.deleteOne({ order_uuid: value.order_uuid }, value);
         let data = await OrderCompleted.create(value);
         if (data) response.push(data);
