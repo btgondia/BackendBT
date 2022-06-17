32e99d6783dd10f45d807d8d3af9f7049736ae60
M Waseem <wasirana80@gmail.com>
2022-06-17 18:44:58 +0500

    Update Item.js

--- Models/Item.js
+++ Models/Item.js
@@ -1,52 +1,60 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-    item_title: {
-        type: String,
-    },
-    item_discount: {
-        type: String,
-    },
-   sort_order:{
-       type:String
-   },
-   free_issue:{
-       type:String
-   },
-    item_uuid: {
-        type: String,
-    },
-    one_pack: {
-        type: String,
-    },
-    company_uuid: {
-        type: String,
-    },
-    category_uuid: {
-        type: String,
-    },
-    pronounce: {
-        type: String,
-    },
-    mrp: {
-        type: String,
-    },
-    item_price: {
-        type: String,
-    },
-    item_gst: {
-        type: String,
-    },
-    conversion: {
-        type: String,
-    },
-    barcode: [{
-        type: String,
-    }],
-    item_group_uuid: [{
-        type: String,
-    }],
-})
+  item_title: {
+    type: String,
+  },
+  item_discount: {
+    type: String,
+  },
 
+  status: {
+    type: Number,
+  },
 
-module.exports = mongoose.model('items', itemSchema)
+  sort_order: {
+    type: String,
+  },
+  free_issue: {
+    type: String,
+  },
+  item_uuid: {
+    type: String,
+  },
+  one_pack: {
+    type: String,
+  },
+  company_uuid: {
+    type: String,
+  },
+  category_uuid: {
+    type: String,
+  },
+  pronounce: {
+    type: String,
+  },
+  mrp: {
+    type: String,
+  },
+  item_price: {
+    type: String,
+  },
+  item_gst: {
+    type: String,
+  },
+  conversion: {
+    type: String,
+  },
+  barcode: [
+    {
+      type: String,
+    },
+  ],
+  item_group_uuid: [
+    {
+      type: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("items", itemSchema);
