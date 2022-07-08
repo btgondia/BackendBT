d904296787595990da3977861663c86448f7923c
M Waseem <wasirana80@gmail.com>
2022-07-08 11:30:34 +0500

    Update Orders.js

--- Models/Orders.js
+++ Models/Orders.js
@@ -20,6 +20,7 @@ const OrdersSchema = new mongoose.Schema({
       status: { type: Number },
       unit_price: { type: Number },
       old_price: { type: Number },
+      price_approval: { type: String },
       gst_percentage: { type: Number },
       item_total: { type: Number },
       free: { type: Number },
