887a586b317f7e94f6af97716bd6d773bd759d50
M Waseem <wasirana80@gmail.com>
2022-08-16 18:49:34 +0500

    task

---
+++ Models/Tasks.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  task_uuid: {
+    type: String,
+  },
+  counter_uuid: {
+    type: String,
+  },
+  created_by: {
+    type: String,
+  },
+  created_at: {
+    type: Number,
+  },
+  status: {
+    type: Number,
+  },
+  completed_at: {
+    type: Number,
+  },
+  completed_by: {
+    type: String,
+  },
+  task: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("tasks", taskSchema);
---
+++ Routes/Tasks.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const router = express.Router();
+const { v4: uuid } = require("uuid");
+const Tasks = require("../Models/Tasks");
+
+router.post("/postTask", async (req, res) => {
+  try {
+    let value = req.body;
+    if (!value) res.json({ success: false, message: "Invalid Data" });
+    let time = new Date();
+    value = {
+      ...value,
+      created_at: time.getTime(),
+      task_uuid: uuid(),
+      status: 0,
+    };
+
+    console.log(value);
+    let response = await Tasks.create(value);
+    if (response) {
+      res.json({ success: true, result: response });
+    } else res.json({ success: false, message: "Task Not created" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+
+router.get("/GetTasksList", async (req, res) => {
+  try {
+    let data = await Tasks.find({});
+
+    if (data.length)
+      res.json({ success: true, result: data.filter((a) => +a.status === 0) });
+    else res.json({ success: false, message: "Task Not found" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+
+router.put("/putTask", async (req, res) => {
+  try {
+    let result = [];
+    for (let value of req.body) {
+      if (!value) res.json({ success: false, message: "Invalid Data" });
+      value = Object.keys(value)
+        .filter((key) => key !== "_id")
+        .reduce((obj, key) => {
+          obj[key] = value[key];
+          return obj;
+        }, {});
+      console.log(value);
+      let time = new Date();
+      value = {
+        ...value,
+        completed_at: time.getTime(),
+      };
+      let response = await Tasks.updateOne(
+        { task_uuid: value.task_uuid },
+        value
+      );
+      if (response.acknowledged) {
+        result.push({ success: true, result: value });
+      } else result.push({ success: false, message: "Task Not created" });
+    }
+    res.json({ success: true, result });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+
+module.exports = router;
