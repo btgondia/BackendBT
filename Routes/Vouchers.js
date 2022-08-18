5ee15b1acbc357b3ff92dc4cc5be3c54e557294f
M Waseem <wasirana80@gmail.com>
2022-08-18 16:00:30 +0500

    Update Vouchers.js

--- Routes/Vouchers.js
+++ Routes/Vouchers.js
@@ -90,10 +90,10 @@ router.post("/postVoucher", async (req, res) => {
   }
 });
 
-router.get("/GetItemList", async (req, res) => {
+router.get("/GetPendingVoucharsList/:delivered", async (req, res) => {
   try {
-    let data = await Item.find({});
-
+    let data = await Vochers.find({delivered:req.params.delivered});
+console.log(data)
     if (data.length) res.json({ success: true, result: data });
     else res.json({ success: false, message: "Item Not found" });
   } catch (err) {
@@ -101,30 +101,88 @@ router.get("/GetItemList", async (req, res) => {
   }
 });
 
-router.put("/putItem", async (req, res) => {
-  try {
-    let result = [];
-    for (let value of req.body) {
-      if (!value) res.json({ success: false, message: "Invalid Data" });
-      value = Object.keys(value)
-        .filter((key) => key !== "_id")
-        .reduce((obj, key) => {
-          obj[key] = value[key];
-          return obj;
-        }, {});
-      console.log(value);
-      let response = await Item.updateOne(
-        { item_uuid: value.item_uuid },
-        value
-      );
-      if (response.acknowledged) {
-        result.push({ success: true, result: value });
-      } else result.push({ success: false, message: "Item Not created" });
-    }
-    res.json({ success: true, result });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err });
-  }
+router.put("/ConfirmVoucher", async (req, res) => {
+    try {
+        let value = req.body;
+        if (!value) res.json({ success: false, message: "Invalid Data" });
+        let voucherData= await Vochers.findOne({voucher_uuid:value.voucher_uuid})
+       voucherData=JSON.parse(JSON.stringify(voucherData))
+    
+        console.log(voucherData);
+        for (let item of voucherData.item_details) {
+          let itemData = await Item.findOne({
+            item_uuid: item.item_uuid,
+          });
+          itemData = JSON.parse(JSON.stringify(itemData));
+          let stock = itemData.stock;
+          console.log("Stock", stock);
+          let qty = +item.b * +itemData.conversion + item.p;
+          stock = stock?.filter((a) => a.warehouse_uuid === voucherData.from_warehouse)
+            ?.length
+            ? stock.map((a) =>
+                a.warehouse_uuid === voucherData.from_warehouse
+                  ? { ...a, qty: a.qty - qty }
+                  : a
+              )
+            : stock?.length
+            ? +voucherData.from_warehouse === 0
+              ? stock || []
+              : [
+                  ...stock,
+                  {
+                    warehouse_uuid: voucherData.from_warehouse,
+                    min_level: 0,
+                    qty: -qty,
+                  },
+                ]
+            : [
+                {
+                  warehouse_uuid: voucherData.from_warehouse,
+                  min_level: 0,
+                  qty: -qty,
+                },
+              ];
+          console.log("Stock", stock);
+          stock = stock?.filter((a) => a.warehouse_uuid === voucherData.to_warehouse)
+            ?.length
+            ? stock.map((a) =>
+                a.warehouse_uuid === voucherData.to_warehouse
+                  ? { ...a, qty: a.qty + qty }
+                  : a
+              )
+            : stock?.length
+            ? +voucherData.from_warehouse === 0
+              ? stock || []
+              : [
+                  ...stock,
+                  {
+                    warehouse_uuid: voucherData.to_warehouse,
+                    min_level: 0,
+                    qty: +qty,
+                  },
+                ]
+            : [
+                {
+                  warehouse_uuid: voucherData.to_warehouse,
+                  min_level: 0,
+                  qty: +qty,
+                },
+              ];
+          console.log("Stock", stock);
+          await Item.updateOne(
+            {
+              item_uuid: item.item_uuid,
+            },
+            { stock }
+          );
+        }
+        let response = await Vochers.updateMany({voucher_uuid:value.voucher_uuid},{delivered:1});
+        if (response) {
+          res.json({ success: true, result: response });
+        } else res.json({ success: false, message: "Item Not created" });
+      } catch (err) {
+        res.status(500).json({ success: false, message: err });
+      }
 });
 
 module.exports = router;
