948f31df06e23184f512f4e9b1342ba3eb1b4510
M Waseem <wasirana80@gmail.com>
2022-08-22 18:54:23 +0500

    Update Warehouse.js

--- Routes/Warehouse.js
+++ Routes/Warehouse.js
@@ -71,6 +71,7 @@ router.get("/GetSuggestionItemsList/:warehouse_uuid", async (req, res) => {
         let obj = {
           ...item,
           stock: itemData.stock,
+          item_title: itemData.item_title,
           conversion: itemData.conversion,
           b: parseInt(+b + +p / +itemData?.conversion),
           p: parseInt(+p % +itemData?.conversion),
