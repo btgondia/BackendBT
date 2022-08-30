a4b83318d83c37d3f5cef6fb92dc84a6e78986df
M Waseem <wasirana80@gmail.com>
2022-08-30 16:55:32 +0500

    Update Vouchers.js

--- Routes/Vouchers.js
+++ Routes/Vouchers.js
@@ -108,89 +108,89 @@ router.get("/GetPendingVoucharsList/:delivered", async (req, res) => {
 
 router.put("/ConfirmVoucher", async (req, res) => {
   try {
-    let value = req.body;
-    if (!value) res.json({ success: false, message: "Invalid Data" });
-    let voucherData = await Vochers.findOne({
-      voucher_uuid: value.voucher_uuid,
-    });
-    voucherData = JSON.parse(JSON.stringify(voucherData));
+  let value = req.body;
+  if (!value) res.json({ success: false, message: "Invalid Data" });
+  let voucherData = await Vochers.findOne({
+    voucher_uuid: value.voucher_uuid,
+  });
+  voucherData = JSON.parse(JSON.stringify(voucherData));
 
-    console.log(voucherData);
-    for (let item of voucherData.item_details) {
-      let itemData = await Item.findOne({
-        item_uuid: item.item_uuid,
-      });
-      itemData = JSON.parse(JSON.stringify(itemData));
-      let stock = itemData.stock||[];
-      console.log("Stock", stock);
-      let qty = +item.b * +itemData.conversion + item.p;
-      stock =
-        +voucherData.from_warehouse === 0
-          ? stock
-          : stock?.filter(
-              (a) => a.warehouse_uuid === voucherData.from_warehouse
-            )?.length
-          ? stock.map((a) =>
-              a.warehouse_uuid === voucherData.from_warehouse
-                ? { ...a, qty: a.qty - qty }
-                : a
-            )
-          : stock?.length
-          ? [
-              ...stock,
-              {
-                warehouse_uuid: voucherData.from_warehouse,
-                min_level: 0,
-                qty: -qty,
-              },
-            ]
-          : [
-              {
-                warehouse_uuid: voucherData.from_warehouse,
-                min_level: 0,
-                qty: -qty,
-              },
-            ];
-      console.log("Stock", stock);
-      stock = stock?.filter(
-        (a) => a.warehouse_uuid === voucherData.to_warehouse
-      )?.length
-        ? stock.map((a) =>
-            a.warehouse_uuid === voucherData.to_warehouse
-              ? { ...a, qty: a.qty + qty }
+  console.log(voucherData);
+  for (let item of voucherData.item_details) {
+    let itemData = await Item.findOne({
+      item_uuid: item.item_uuid,
+    });
+    itemData = JSON.parse(JSON.stringify(itemData));
+    if (!itemData) return;
+    let stock = itemData?.stock || [];
+    console.log("Stock", stock, item.item_uuid);
+    let qty = +item.b * +itemData.conversion + item.p;
+    stock =
+      +voucherData.from_warehouse === 0
+        ? stock
+        : stock?.filter((a) => a.warehouse_uuid === voucherData.from_warehouse)
+            ?.length
+        ? stock?.map((a) =>
+            a.warehouse_uuid === voucherData.from_warehouse
+              ? { ...a, qty: a.qty - qty }
               : a
           )
         : stock?.length
         ? [
             ...stock,
             {
-              warehouse_uuid: voucherData.to_warehouse,
+              warehouse_uuid: voucherData.from_warehouse,
               min_level: 0,
-              qty: +qty,
+              qty: -qty,
             },
           ]
         : [
             {
-              warehouse_uuid: voucherData.to_warehouse,
+              warehouse_uuid: voucherData.from_warehouse,
               min_level: 0,
-              qty: +qty,
+              qty: -qty,
             },
           ];
-      console.log("Stock", stock);
+    console.log("Stock", stock);
+    stock = stock?.filter((a) => a.warehouse_uuid === voucherData.to_warehouse)
+      ?.length
+      ? stock.map((a) =>
+          a.warehouse_uuid === voucherData.to_warehouse
+            ? { ...a, qty: a.qty + qty }
+            : a
+        )
+      : stock?.length
+      ? [
+          ...stock,
+          {
+            warehouse_uuid: voucherData.to_warehouse,
+            min_level: 0,
+            qty: +qty,
+          },
+        ]
+      : [
+          {
+            warehouse_uuid: voucherData.to_warehouse,
+            min_level: 0,
+            qty: +qty,
+          },
+        ];
+    console.log("Stock", stock);
+    if (stock.length)
       await Item.updateOne(
         {
           item_uuid: item.item_uuid,
         },
         { stock }
       );
-    }
-    let response = await Vochers.updateMany(
-      { voucher_uuid: value.voucher_uuid },
-      { delivered: 1 }
-    );
-    if (response) {
-      res.json({ success: true, result: response });
-    } else res.json({ success: false, message: "Item Not created" });
+  }
+  let response = await Vochers.updateMany(
+    { voucher_uuid: value.voucher_uuid },
+    { delivered: 1 }
+  );
+  if (response) {
+    res.json({ success: true, result: response });
+  } else res.json({ success: false, message: "Item Not created" });
   } catch (err) {
     res.status(500).json({ success: false, message: err });
   }
