bf87694ddc077f7933a02635f27f088eaf3d02ad
M Waseem <wasirana80@gmail.com>
2022-09-01 16:53:32 +0500

    Update Receipts.js

--- Routes/Receipts.js
+++ Routes/Receipts.js
@@ -60,27 +60,33 @@ router.put("/putReceipt", async (req, res) => {
   }
 });
 router.put("/putReceiptUPIStatus", async (req, res) => {
-  try {
-    let value = req.body;
-    if (!value) res.json({ success: false, message: "Invalid Data" });
+  // try {
+  let value = req.body;
+  if (!value) res.json({ success: false, message: "Invalid Data" });
 
-    console.log(value);
-    let response = await Receipts.findOne({ order_uuid: value.order_uuid });
-    response = JSON.parse(JSON.stringify(response));
-    response = response.modes.map((a) =>
-      a.mode_uuid === value.mode_uuid ? { ...a, status: value.status } : a
-    );
-    console.log(response);
-    let data = await Receipts.updateMany(
+  let response = await Receipts.findOne({
+    $or: [
+      { invoice_number: value.invoice_number },
       { order_uuid: value.order_uuid },
-      { modes: response }
-    );
-    if (data.acknowledged) {
-      res.json({ success: true, result: response });
-    } else res.json({ success: false, message: "Receipts Not created" });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err });
-  }
+    ],
+  });
+  console.log(response);
+
+  response = JSON.parse(JSON.stringify(response));
+  response = response.modes.map((a) =>
+    a.mode_uuid === value.mode_uuid ? { ...a, status: value.status } : a
+  );
+  console.log(response);
+  let data = await Receipts.updateMany(
+    { order_uuid: value.order_uuid },
+    { modes: response }
+  );
+  if (data.acknowledged) {
+    res.json({ success: true, result: response });
+  } else res.json({ success: false, message: "Receipts Not created" });
+  // } catch (err) {
+  //   res.status(500).json({ success: false, message: err });
+  // }
 });
 router.get("/getReceipt", async (req, res) => {
   try {
@@ -89,30 +95,21 @@ router.get("/getReceipt", async (req, res) => {
     let usersData = await Users.find({
       user_uuid: { $in: response.map((a) => a.user_uuid).filter((a) => a) },
     });
-    response = response
-      .map((a) =>
-        a.modes.filter(
-          (b) =>
-            b.mode_uuid === "c67b5988-d2b6-11ec-9d64-0242ac120002" &&
-            b.amt &&
-            b.status === 0
-        ).length
-          ? { ...a, mode_title: "UPI" }
-          : a.modes.filter(
-              (b) =>
-                b.mode_uuid === "c67b5794-d2b6-11ec-9d64-0242ac120002" &&
-                b.amt &&
-                b.status === 0
-            ).length
-          ? { ...a, mode_title: "Cheque" }
-          : a
-      )
-      .filter((a) => a.mode_title);
-
-    console.log(response);
+    console.log(response.length);
+    response = response.filter(
+      (a) => a.modes.filter((b) => b.status === 0 && b.amt).length
+    );
+    console.log(response.length);
     if (response.length) {
       let data = [];
       for (let item of response) {
+        let modes = item.modes.filter(
+          (a) =>
+            (a.mode_uuid === "c67b5988-d2b6-11ec-9d64-0242ac120002" ||
+              a.mode_uuid === "c67b5794-d2b6-11ec-9d64-0242ac120002") &&
+            a.amt &&
+            a.status === 0
+        );
         let orderData = await OrderCompleted.findOne({
           order_uuid: item.order_uuid,
         });
@@ -123,22 +120,21 @@ router.get("/getReceipt", async (req, res) => {
           let counterData = await Counters.findOne({
             counter_uuid: orderData.counter_uuid,
           });
-          console.log(counterData);
-          let obj = {
-            mode_title: item.mode_title,
-            mode_uuid: item.mode_uuid,
-            counter_title: counterData?.counter_title || "",
-            invoice_number: orderData?.invoice_number || "",
-            order_date: orderData?.status?.find((a) => +a?.stage === 1)?.time,
-            payment_date: item?.time,
-            order_uuid: item.order_uuid,
-            user_title: usersData.find((a) => item.user_uuid === a.user_uuid)
-              ?.user_title,
-            amt: item.modes.find(
-              (a) => a.mode_uuid === "c67b5988-d2b6-11ec-9d64-0242ac120002"
-            )?.amt,
-          };
-          data.push(obj);
+          for (let item1 of modes) {
+            let obj = {
+              mode_title: item1.mode_uuid==="c67b5988-d2b6-11ec-9d64-0242ac120002"?"UPI":"Cheque",
+              mode_uuid: item1.mode_uuid,
+              counter_title: counterData?.counter_title || "",
+              invoice_number: orderData?.invoice_number || "",
+              order_date: orderData?.status?.find((a) => +a?.stage === 1)?.time,
+              payment_date: item?.time,
+              order_uuid: item.order_uuid,
+              user_title: usersData.find((a) => item.user_uuid === a.user_uuid)
+                ?.user_title,
+              amt: item1?.amt,
+            };
+            data.push(obj);
+          }
         }
       }
       res.json({ success: true, result: data });
