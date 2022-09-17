ce5de247c45aefbe29144de357e4826bdea0a29e
M Waseem <wasirana80@gmail.com>
2022-09-17 12:46:51 +0500

    Update Vouchers.js

--- Routes/Vouchers.js
+++ Routes/Vouchers.js
@@ -19,73 +19,7 @@ router.post("/postVoucher", async (req, res) => {
     };
 
     console.log(value);
-    // for (let item of value.item_details) {
-    //   let itemData = await Item.findOne({
-    //     item_uuid: item.item_uuid,
-    //   });
-    //   itemData = JSON.parse(JSON.stringify(itemData));
-    //   let stock = itemData.stock;
-    //   console.log("Stock", stock);
-    //   let qty = +item.b * +itemData.conversion + item.p;
-    //   stock = stock?.filter((a) => a.warehouse_uuid === value.from_warehouse)
-    //     ?.length
-    //     ? stock.map((a) =>
-    //         a.warehouse_uuid === value.from_warehouse
-    //           ? { ...a, qty: a.qty - qty }
-    //           : a
-    //       )
-    //     : stock?.length
-    //     ? +value.from_warehouse === 0
-    //       ? stock || []
-    //       : [
-    //           ...stock,
-    //           {
-    //             warehouse_uuid: value.from_warehouse,
-    //             min_level: 0,
-    //             qty: -qty,
-    //           },
-    //         ]
-    //     : [
-    //         {
-    //           warehouse_uuid: value.from_warehouse,
-    //           min_level: 0,
-    //           qty: -qty,
-    //         },
-    //       ];
-    //   console.log("Stock", stock);
-    //   stock = stock?.filter((a) => a.warehouse_uuid === value.to_warehouse)
-    //     ?.length
-    //     ? stock.map((a) =>
-    //         a.warehouse_uuid === value.to_warehouse
-    //           ? { ...a, qty: a.qty + qty }
-    //           : a
-    //       )
-    //     : stock?.length
-    //     ? +value.from_warehouse === 0
-    //       ? stock || []
-    //       : [
-    //           ...stock,
-    //           {
-    //             warehouse_uuid: value.to_warehouse,
-    //             min_level: 0,
-    //             qty: +qty,
-    //           },
-    //         ]
-    //     : [
-    //         {
-    //           warehouse_uuid: value.to_warehouse,
-    //           min_level: 0,
-    //           qty: +qty,
-    //         },
-    //       ];
-    //   console.log("Stock", stock);
-    //   await Item.updateOne(
-    //     {
-    //       item_uuid: item.item_uuid,
-    //     },
-    //     { stock }
-    //   );
-    // }
+
     let response = await Vochers.create(value);
     if (response) {
       res.json({ success: true, result: response });
