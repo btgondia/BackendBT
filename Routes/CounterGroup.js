7a802039d3970a6b921604567856c43c12d67819
M Waseem <wasirana80@gmail.com>
2023-01-27 17:40:26 +0500

    fixes

--- Routes/CounterGroup.js
+++ Routes/CounterGroup.js
@@ -3,16 +3,16 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuid } = require("uuid");
 const CounterGroup = require("../Models/CounterGroup");
-
+const Counters = require("../Models/Counters");
 
 router.post("/postCounterGroup", async (req, res) => {
   try {
     let value = req.body;
     if (!value) res.json({ success: false, message: "Invalid Data" });
-    value = {...value,counter_group_uuid:uuid()};
+    value = { ...value, counter_group_uuid: uuid() };
 
     console.log(value);
-    let response = await CounterGroup.create( value );
+    let response = await CounterGroup.create(value);
     if (response) {
       res.json({ success: true, result: response });
     } else res.json({ success: false, message: "Group Not created" });
@@ -36,18 +36,54 @@ router.put("/putCounterGroup", async (req, res) => {
   try {
     let value = req.body;
     if (!value) res.json({ success: false, message: "Invalid Data" });
-    value= Object.keys(value)
-    .filter((key) => key !== "_id")
-    .reduce((obj, key) => {
-      obj[key] = value[key];
-      return obj;
-    }, {})
+    value = Object.keys(value)
+      .filter((key) => key !== "_id")
+      .reduce((obj, key) => {
+        obj[key] = value[key];
+        return obj;
+      }, {});
 
     console.log(value);
-    let response = await CounterGroup.updateOne({counter_group_uuid:value.counter_group_uuid}, value );
+    let response = await CounterGroup.updateOne(
+      { counter_group_uuid: value.counter_group_uuid },
+      value
+    );
     if (response.acknowledged) {
       res.json({ success: true, result: value });
     } else res.json({ success: false, message: "Group Not updated" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+router.delete("/deleteCounterGroup", async (req, res) => {
+  try {
+    let { counter_group_uuid } = req.body;
+    if (!counter_group_uuid)
+      res.json({ success: false, message: "Invalid Data" });
+
+    let counterData = await Counters.find({
+      counter_group_uuid,
+    });
+
+    if (counterData.length) {
+      for (let counter of counterData) {
+        await Counters.updateMany(
+          { counter_uuid: counter.counter_uuid },
+          {
+            counter_group_uuid: item.counter_group_uuid.filter(
+              (a) => a !== counter_group_uuid
+            ),
+          }
+        );
+      }
+    }
+    let response = await CounterGroup.deleteMany({ counter_group_uuid });
+    if (response.acknowledged) {
+      res.json({ success: true, result: response });
+    } else
+      res
+        .status(404)
+        .json({ success: false, message: "Counter Group Not Deleted" });
   } catch (err) {
     res.status(500).json({ success: false, message: err });
   }
--- Routes/ItemGroup.js
+++ Routes/ItemGroup.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 const { v4: uuid } = require("uuid");
+const Item = require("../Models/Item");
 const ItemGroup = require("../Models/ItemGroup");
-
 
 router.post("/postItemGroup", async (req, res) => {
   try {
     let value = req.body;
     if (!value) res.json({ success: false, message: "Invalid Data" });
-    value = {...value,item_group_uuid:uuid()};
+    value = { ...value, item_group_uuid: uuid() };
 
     console.log(value);
-    let response = await ItemGroup.create( value );
+    let response = await ItemGroup.create(value);
     if (response) {
       res.json({ success: true, result: response });
     } else res.json({ success: false, message: "Group Not created" });
@@ -24,14 +24,17 @@ router.put("/putItemGroup", async (req, res) => {
   try {
     let value = req.body;
     if (!value) res.json({ success: false, message: "Invalid Data" });
-    value= Object.keys(value)
-    .filter((key) => key !== "_id")
-    .reduce((obj, key) => {
-      obj[key] = value[key];
-      return obj;
-    }, {})
+    value = Object.keys(value)
+      .filter((key) => key !== "_id")
+      .reduce((obj, key) => {
+        obj[key] = value[key];
+        return obj;
+      }, {});
     console.log(value);
-    let response = await ItemGroup.updateOne( {item_group_uuid:value.item_group_uuid},value );
+    let response = await ItemGroup.updateOne(
+      { item_group_uuid: value.item_group_uuid },
+      value
+    );
     if (response) {
       res.json({ success: true, result: response });
     } else res.json({ success: false, message: "Group Not updated" });
@@ -39,7 +42,36 @@ router.put("/putItemGroup", async (req, res) => {
     res.status(500).json({ success: false, message: err });
   }
 });
+router.delete("/deleteItemGroup", async (req, res) => {
+  try {
+    let { item_group_uuid } = req.body;
+    if (!item_group_uuid) res.json({ success: false, message: "Invalid Data" });
+  
+    let itemData = await Item.find({
+      item_group_uuid,
+    });
 
+    if (itemData.length) {
+      for (let item of itemData) {
+        await Item.updateMany(
+          { item_uuid: item.item_uuid },
+          {
+            item_group_uuid: item.item_group_uuid.filter(
+              (a) => a !== item_group_uuid
+            ),
+          }
+        );
+      }
+    }
+    let response = await ItemGroup.deleteMany({item_group_uuid})
+    if (response.acknowledged) {
+      res.json({ success: true, result: response });
+    } else
+      res.status(404).json({ success: false, message: "Item Not Deleted" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
 router.get("/GetItemGroupList", async (req, res) => {
   try {
     let data = await ItemGroup.find({});
