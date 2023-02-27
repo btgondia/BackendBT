a58f010723655cc706bfa4a70252a4fbb1f66df7
Jatin Sharma <jatin2907sharma@gmail.com>
2023-02-28 00:19:17 +0530

    timestamps on logs

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const App = require("./App.js")
+const App = require("./App.js");
+
+console.logCopy = console.log.bind(console);
+console.log = function (...data) {
+  var currentDate = "[" + new Date().toUTCString() + "] ";
+  this.logCopy(currentDate, ...data);
+};
 
 App.listen(9000, () => console.log("Server running on port 9000"));
