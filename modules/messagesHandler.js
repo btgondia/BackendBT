803e54d502fcf401d7706106afeef0d235a5b90b
Jatin Sharma <jatin2907sharma@gmail.com>
2023-04-26 23:45:02 +0530

    sendPDF, Order By Inv No

--- modules/messagesHandler.js
+++ modules/messagesHandler.js
@@ -1,43 +1,44 @@
-const fs = require("fs");
-const Notification_logs = require("../Models/notification_log");
-const { getFileName, generatePDFs } = require("./puppeteerUtilities");
-const { messageEnque } = require("../queues/messageQueue");
+const fs = require("fs")
+const Notification_logs = require("../Models/notification_log")
+const { getFileName, generatePDFs } = require("./puppeteerUtilities")
+const { messageEnque } = require("../queues/messageQueue")
 const filterContacts = coll =>
-	coll?.filter(i => i?.mobile && i?.lable?.find(a => a.type === "wa" && +a.varification));
+	coll?.filter(i => i?.mobile && i?.lable?.find(a => a.type === "wa" && +a.varification))
 
 const contactsProcessHandler = async (contacts, messagesCollection, counterData, value, options = {}) => {
 	for (let contact of contacts) {
-		if (!contact.mobile) continue;
+		if (!contact?.mobile) continue
 
-		let messages = [];
-		if (`${contact.mobile}`?.length === 10) contact.mobile = `91${contact.mobile}`;
+		let messages = []
+		if (`${contact.mobile}`?.length === 10) contact.mobile = `91${contact.mobile}`
 
-		for (let messageobj of messagesCollection) {
-			let message = "";
-			if (messageobj?.type === "text") {
-				message = messageobj.text
-					?.replace(/{counter_title}/g, counterData?.counter_title || "")
-					?.replace(
-						/{short_link}/g,
-						"https://btgondia.com/counter/" + counterData?.short_link || ""
-					)
-					?.replace(/{invoice_number}/g, value?.invoice_number || "")
-					?.replace(/{amount}/g, value.order_grandtotal || value?.amount || value?.amt || "");
+		if (messagesCollection)
+			for (let messageobj of messagesCollection) {
+				let message = ""
+				if (messageobj?.type === "text") {
+					message = messageobj.text
+						?.replace(/{counter_title}/g, counterData?.counter_title || "")
+						?.replace(
+							/{short_link}/g,
+							"https://btgondia.com/counter/" + counterData?.short_link || ""
+						)
+						?.replace(/{invoice_number}/g, value?.invoice_number || "")
+						?.replace(/{amount}/g, value.order_grandtotal || value?.amount || value?.amt || "")
 
-				const doc = { number: `${contact.mobile}`, type: "text", message };
-				messages.push(doc);
-				await messageEnque(doc);
-			} else if (fs.existsSync("./uploads/" + (messageobj.uuid || "") + ".png")) {
-				const doc = {
-					number: `${contact.mobile}`,
-					type: "media",
-					filename: messageobj.uuid + ".png",
-					message: messageobj?.caption || "",
-				};
-				messages.push(doc);
-				await messageEnque(doc);
+					const doc = { number: `${contact.mobile}`, type: "text", message }
+					messages.push(doc)
+					await messageEnque(doc)
+				} else if (fs.existsSync("./uploads/" + (messageobj.uuid || "") + ".png")) {
+					const doc = {
+						number: `${contact.mobile}`,
+						type: "media",
+						filename: messageobj.uuid + ".png",
+						message: messageobj?.caption || "",
+					}
+					messages.push(doc)
+					await messageEnque(doc)
+				}
 			}
-		}
 
 		if (options.notify && value?.order_uuid && fs.existsSync(`./uploads/${getFileName(value)}`)) {
 			const doc = {
@@ -45,9 +46,10 @@ const contactsProcessHandler = async (contacts, messagesCollection, counterData,
 				type: "media",
 				filename: getFileName(value),
 				message: value.invoice_number || "",
-			};
-			messages.push(doc);
-			await messageEnque(doc);
+			}
+			console.log("sending message:", doc)
+			messages.push(doc)
+			await messageEnque(doc)
 		}
 
 		await Notification_logs.create({
@@ -56,21 +58,21 @@ const contactsProcessHandler = async (contacts, messagesCollection, counterData,
 			messages: messages?.filter(i => i.message)?.map(i => ({ text: i.message })),
 			invoice_number: value.invoice_number,
 			created_at: new Date().getTime(),
-		});
+		})
 	}
-};
+}
 
 const sendMessages = async ({ counterData = {}, value = {}, WhatsappNotification }) => {
 	try {
-		const filename = getFileName(value);
-		const filepath = `uploads/${filename}`;
+		const filename = getFileName(value)
+		const filepath = `uploads/${filename}`
 
 		if (
 			(!WhatsappNotification || (WhatsappNotification && WhatsappNotification?.checkbox)) &&
 			value?.order_uuid &&
 			!fs.existsSync(filepath)
 		) {
-			await generatePDFs([{ filename, order_id: value.order_uuid }]);
+			await generatePDFs([{ filename, order_id: value.order_uuid }])
 		}
 
 		if (WhatsappNotification?.message) {
@@ -80,16 +82,16 @@ const sendMessages = async ({ counterData = {}, value = {}, WhatsappNotification
 				counterData,
 				value,
 				{ notify: WhatsappNotification?.checkbox }
-			);
+			)
 		}
 	} catch (err) {
-		console.log(err);
+		console.log(err)
 	}
-};
+}
 
-const compaignShooter = async ({ counterData = {}, value = {} }) => {
-	const contacts = filterContacts(counterData?.mobile || []).concat(value?.mobile);
-	await contactsProcessHandler(contacts, value.message, counterData, value);
-};
+const compaignShooter = async ({ counterData = {}, value = {}, options }) => {
+	const contacts = filterContacts(counterData?.mobile || []).concat(value?.mobile)
+	await contactsProcessHandler(contacts, value.message, counterData, value, options)
+}
 
-module.exports = { sendMessages, compaignShooter };
+module.exports = { sendMessages, compaignShooter }
