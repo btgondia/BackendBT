fb55233e5b3ef385501cf01b394302a992e20463
Jatin Sharma <jatin2907sharma@gmail.com>
2023-06-08 01:57:31 +0530

    companies crud routes

--- Models/Companies.js
+++ Models/Companies.js
@@ -1,20 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const CompaniesSchema = new mongoose.Schema({
-    company_title: {
-        type: String,
-    },
-    sort_order: {
-        type: Number,
-
-    },
-    company_uuid: {
-        type: String,
-
-    },
-    
-
+	company_uuid: { type: String },
+	company_title: { type: String },
+	sort_order: { type: Number },
+	status: { type: Number, default: 1 },
 })
 
-
-module.exports = mongoose.model('companies', CompaniesSchema)
+module.exports = mongoose.model("companies", CompaniesSchema)
--- Routes/Companies.js
+++ Routes/Companies.js
@@ -1,20 +1,49 @@
-const express = require("express");
-
-const router = express.Router();
-const { v4: uuid } = require("uuid");
-const Companies = require("../Models/Companies");
-
-
+const express = require("express")
+const router = express.Router()
+const Companies = require("../Models/Companies")
 
 router.get("/getCompanies", async (req, res) => {
-  try {
-    let data = await Companies.find({});
+	try {
+		const data = await Companies.find({})
+		if (data.length) res.json({ success: true, result: data })
+		else res.json({ success: false, message: "Companies Not found" })
+	} catch (err) {
+		res.status(500).json({ message: err?.message })
+	}
+})
 
-    if (data.length) res.json({ success: true, result: data });
-    else res.json({ success: false, message: "Companies Not found" });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err });
-  }
-});
+router.put("/", async (req, res) => {
+	try {
+		const payload = req.body
+		delete payload?._id
+		const result = await Companies.findOneAndUpdate({ company_uuid: payload?.company_uuid }, payload)
+		if (result) res.json({ success: true })
+		else res.status(409).json({ message: "Failed to update." })
+	} catch (err) {
+		res.status(500).json({ message: err?.message })
+	}
+})
 
-module.exports = router;
+router.post("/", async (req, res) => {
+	try {
+		const payload = req.body
+		const result = await Companies.create(payload)
+		if (result) res.json({ success: true })
+		else res.status(409).json({ message: "Failed to create." })
+	} catch (err) {
+		res.status(500).json({ message: err?.message })
+	}
+})
+
+router.delete("/", async (req, res) => {
+	try {
+		const { company_uuid } = req.body
+		const result = await Companies.findOneAndDelete({ company_uuid })
+		if (result) res.json({ success: true })
+		else res.status(409).json({ message: "Failed to delete." })
+	} catch (err) {
+		res.status(500).json({ message: err?.message })
+	}
+})
+
+module.exports = router
