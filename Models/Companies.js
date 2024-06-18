e442b181a0da4430b6fa50c64b05dac3d547c063
unknown <ujjgpta9@gmail.com>
2024-06-18 23:10:27 +0530

    added new attribute comapny_color in Companies model

--- Models/Companies.js
+++ Models/Companies.js
@@ -4,6 +4,7 @@ const CompaniesSchema = new mongoose.Schema({
 	company_uuid: { type: String },
 	company_title: { type: String },
 	sort_order: { type: Number },
+	company_color: { type: String, default:"#000000" },
 	status: { type: Number, default: 1 },
 })
 
