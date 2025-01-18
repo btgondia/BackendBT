53220f0f8690c516f9ab3bf6f6b3699defe7a1f2
Jatin Sharma <jatinsharma29703@gmail.com>
2025-01-19 01:04:41 +0530

    remove projection from import prereq deps

--- Routes/_base.js
+++ Routes/_base.js
@@ -10,28 +10,30 @@ baseRouter.post("/invoice-import-prerequisite", async (req, res) => {
 		const result = {}
 		if (dms_counters?.length > 0)
 			result.counters = await Counters.find(
-				{ dms_buyer_id: { $in: dms_counters } },
-				{
-					counter_uuid: 1,
-					dms_buyer_id: 1,
-					item_special_price: 1,
-					item_special_discount: 1,
-					company_discount: 1
-				}
+				{ dms_buyer_id: { $in: dms_counters } }
+				// {
+				// 	counter_uuid: 1,
+				// 	dms_buyer_id: 1,
+				// 	item_special_price: 1,
+				// 	item_special_discount: 1,
+				// 	company_discount: 1
+				// }
 			)
 
 		if (dms_items?.length > 0)
 			result.items = await Item.find(
-				{ $and: [{ dms_erp_ids: { $in: dms_items } }, { status: 1 }] },
-				{
-					item_uuid: 1,
-					dms_erp_ids: 1,
-					conversion: 1,
-					item_gst: 1,
-					item_css: 1,
-					item_price: 1,
-					status: 1
-				}
+				{ $and: [{ dms_erp_ids: { $in: dms_items } }, { status: 1 }] }
+				// {
+				// 	item_uuid: 1,
+				// 	dms_erp_ids: 1,
+				// 	conversion: 1,
+				// 	item_gst: 1,
+				// 	item_css: 1,
+				// 	item_price: 1,
+				// 	item_discount: 1,
+				// 	company_uuid: 1,
+				// 	status: 1
+				// }
 			)
 
 		if (dms_users?.length > 0)
@@ -46,7 +48,11 @@ baseRouter.post("/invoice-import-prerequisite", async (req, res) => {
 		if (dms_invoice_numbers?.length > 0)
 			result.existing_invoice_orders = await Orders.find(
 				{ dms_invoice_number: { $in: dms_invoice_numbers } },
-				{ _id: 0, dms_invoice_number: 1, order_uuid: 1 }
+				{
+					_id: 0,
+					dms_invoice_number: 1,
+					order_uuid: 1
+				}
 			)
 
 		res.json(result)
